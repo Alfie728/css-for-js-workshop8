79f611c4be1d1f24a70bbb6c9101f70ab1a014ee
Alfie728 <zegro728@gmail.com>
2024-04-11 23:54:41 +0800

    first commit

---
+++ src/components/MobileMenu/MobileMenu.js
@@ -0,0 +1,161 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import styled, { keyframes } from 'styled-components/macro';
+import { DialogOverlay, DialogContent } from '@reach/dialog';
+
+import { QUERIES, WEIGHTS } from '../../constants';
+
+import UnstyledButton from '../UnstyledButton';
+import Icon from '../Icon';
+import VisuallyHidden from '../VisuallyHidden';
+
+const MobileMenu = ({ isOpen, onDismiss }) => {
+  return (
+    <Wrapper isOpen={isOpen} onDismiss={onDismiss}>
+      <Backdrop />
+      <Content aria-label="Menu">
+        <InnerWrapper>
+          <CloseButton onClick={onDismiss}>
+            <Icon id="close" />
+            <VisuallyHidden>Dismiss menu</VisuallyHidden>
+          </CloseButton>
+          <Filler />
+          <Nav>
+            <NavLink href="/sale">Sale</NavLink>
+            <NavLink href="/new">New&nbsp;Releases</NavLink>
+            <NavLink href="/men">Men</NavLink>
+            <NavLink href="/women">Women</NavLink>
+            <NavLink href="/kids">Kids</NavLink>
+            <NavLink href="/collections">Collections</NavLink>
+          </Nav>
+          <Footer>
+            <SubLink href="/terms">Terms and Conditions</SubLink>
+            <SubLink href="/privacy">Privacy Policy</SubLink>
+            <SubLink href="/contact">Contact Us</SubLink>
+          </Footer>
+        </InnerWrapper>
+      </Content>
+    </Wrapper>
+  );
+};
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+const fadeOut = keyframes`
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+  }
+`;
+
+const slideIn = keyframes`
+  from {
+    transform: translateX(100%)
+  }
+  to {
+    transform: translateX(0);
+  }
+`;
+const slideOut = keyframes`
+  from {
+    transform: translateX(0)
+  }
+  to {
+    transform: translateX(100%);
+  }
+`;
+const Wrapper = styled(DialogOverlay)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: transparent;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const Backdrop = styled.div`
+  inset: 0;
+  position: absolute;
+  background: var(--color-backdrop);
+  animation: ${fadeIn} 500ms;
+  will-change: transform;
+`;
+
+const Content = styled(DialogContent)`
+  --overfill: 16px;
+  position: relative;
+  background: white;
+  width: calc(300px + var(--overfill));
+  height: 100%;
+  margin-right: calc(var(--overfill) * -1);
+  padding: 24px 32px;
+  will-change: transform;
+
+  animation: ${slideIn} 600ms both cubic-bezier(0.17, 0.67, 0.3, 1.17);
+  animation-delay: 300ms;
+`;
+
+const InnerWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  will-change: transform;
+
+  animation: ${fadeIn} 400ms both;
+  animation-delay: 600ms;
+`;
+
+const CloseButton = styled(UnstyledButton)`
+  position: absolute;
+  top: 10px;
+  right: 0;
+  padding: 16px;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+const NavLink = styled.a`
+  color: var(--color-gray-900);
+  font-weight: ${WEIGHTS.medium};
+  text-decoration: none;
+  font-size: 1.125rem;
+  text-transform: uppercase;
+
+  &:first-of-type {
+    color: var(--color-secondary);
+  }
+`;
+
+const Filler = styled.div`
+  flex: 1;
+`;
+const Footer = styled.footer`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+  justify-content: flex-end;
+`;
+
+const SubLink = styled.a`
+  color: var(--color-gray-700);
+  font-size: 0.875rem;
+  text-decoration: none;
+`;
+
+export default MobileMenu;
